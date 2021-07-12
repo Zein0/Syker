d918fbddf8293113108f1fa7eaab370edc68d803
salem berro <salemberro29@gmail.com>
2021-07-12 11:41:47 +0300

    Revert "Pushed by mistake"
    This reverts commit df23b1c6524fc95f6cd46a6a8e3c31d49cc86321.

---
+++ backend/src/models/Shirts.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    shirt: {
+        type: String,
+        required:true
+    },
+    size: {
+        type: String,
+        required: true,
+    }
+});
+module.exports = mongoose.model('Shirts',PostSchema);
