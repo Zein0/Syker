96e01c4626025b67162ef37a1ffeac6b2c78963e
Zein0 <ahmadalzein55@outlook.com>
2021-08-21 16:28:15 +0300

    test phase

--- frontend/src/components/SlideShowBack.js
+++ frontend/src/components/SlideShowBack.js
@@ -1,60 +1,54 @@
-import React from 'react';
-import './SlideshowBack.css';
-import './ImageSlider.css';
+import React from "react";
+import "./SlideshowBack.css";
+import "./ImageSlider.css";
 
-const delay = 3000;
+const delay = 300000;
 
-function SlideshowBack({slides}) {
-  const [current, setIndex] = React.useState(0);
-  const timeoutRef = React.useRef(null);
+function SlideshowBack({ slides }) {
+	const [current, setIndex] = React.useState(0);
+	const timeoutRef = React.useRef(null);
 
-  function resetTimeout() {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  }
+	function resetTimeout() {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
+		}
+	}
 
-  React.useEffect(() => {
-    resetTimeout();
-    timeoutRef.current = setTimeout(
-      () =>
-        setIndex((prevIndex) =>
-          prevIndex === slides.length - 1 ? 0 : prevIndex + 1
-        ),
-      delay
-    );
+	React.useEffect(() => {
+		resetTimeout();
+		timeoutRef.current = setTimeout(
+			() =>
+				setIndex((prevIndex) =>
+					prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+				),
+			delay
+		);
 
-    return () => {
-      resetTimeout();
-    };
-  }, [current]);
+		return () => {
+			resetTimeout();
+		};
+	}, [current]);
 
-  
-  return (
-    <div>
-
-  
-    {slides.map((slide, index) => {
-      return (
-        <div
-          className={index === current ? 'slide active' : 'slide'}
-          key={index}
-        >
-          {index === current && (
-            <img src={slide.image} alt='Automatic-Ad-Slider' className='homeImageback' />
-          )}
-        
-        </div>
-        
-      );
-    })}
-  
-   
-
-
-  </div>
-  
-);
+	return (
+		<div>
+			{slides.map((slide, index) => {
+				return (
+					<div
+						className={index === current ? "slide active" : "slide"}
+						key={index}
+					>
+						{index === current && (
+							<img
+								src={slide.image}
+								alt="Automatic-Ad-Slider"
+								className="homeImageback"
+							/>
+						)}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default SlideshowBack;
